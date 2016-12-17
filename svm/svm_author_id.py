b3a6fba9007cabac8321296c33348774f6c71cfa
Patrick Artounian <partounian@gmail.com>
2016-12-16 22:19:08 -0800

    Run rbf, C=10000 on the entire dataset

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -22,19 +22,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
-features_train = features_train[:len(features_train)/100] 
-labels_train = labels_train[:len(labels_train)/100] 
-
 clf = SVC(kernel='rbf', C=10000)
 
 t0 = time()
 clf.fit(features_train, labels_train) 
 print "training time:", round(time()-t0, 3), "s"
+# linear
 # training time: 234.423s  
+
+# rbf, C=10000
+# training time: 141.445 s
 
 t0 = time()
 pred = clf.predict(features_test)
 print "prediction time:", round(time()-t0, 3), "s"
+# linear
 # prediction time: 21.846s
+
+# rbf, C=10000
+# prediction time: 14.378 s
 
 print accuracy_score(labels_test, pred)
