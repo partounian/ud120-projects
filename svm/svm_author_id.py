9d0409e0e964fdc26dfde0f1aba81b3ea8ebd9bd
Patrick Artounian <partounian@gmail.com>
2016-12-16 22:29:32 -0800

    Make predicitions for test #s 10,26,50

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -13,6 +13,17 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
+# linear
+# training time: 234.423s  
+
+# rbf, C=10000
+# training time: 141.445 s
+
+# linear
+# prediction time: 21.846s
+
+# rbf, C=10000
+# prediction time: 14.378 s
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -22,24 +33,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100]
+
 clf = SVC(kernel='rbf', C=10000)
 
 t0 = time()
 clf.fit(features_train, labels_train) 
-print "training time:", round(time()-t0, 3), "s"
-# linear
-# training time: 234.423s  
-
-# rbf, C=10000
-# training time: 141.445 s
 
 t0 = time()
 pred = clf.predict(features_test)
-print "prediction time:", round(time()-t0, 3), "s"
-# linear
-# prediction time: 21.846s
 
-# rbf, C=10000
-# prediction time: 14.378 s
+ten = pred[10]
+twosix = pred[26]
+fifty = pred[50]
+
+print ten
+print twosix
+print fifty
 
 print accuracy_score(labels_test, pred)
