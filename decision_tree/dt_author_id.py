6f1d9ec8d7a22dab5cfed37cfe33e37434bbb215
Patrick Artounian <partounian@gmail.com>
2016-12-28 20:29:59 -0800

    Print number of features for decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -22,6 +22,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 
+print "Number of features", len(features_train[0])
+
 clf = tree.DecisionTreeClassifier(min_samples_split=40)
 
 clf.fit(features_train, labels_train)
