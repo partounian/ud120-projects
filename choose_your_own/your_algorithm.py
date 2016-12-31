9be5dcfc9c7962ecdfe6fb81a0a9c2e72d0be670
Patrick Artounian <partounian@gmail.com>
2016-12-30 23:32:32 -0800

    Correct generated image and finish your_algorithm

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -6,7 +6,6 @@ from class_vis import prettyPicture
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
-
 ### the training data (features_train, labels_train) have both "fast" and "slow"
 ### points mixed together--separate them so we can give them different colors
 ### in the scatterplot and identify them visually
@@ -14,7 +13,6 @@ grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if l
 bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
 grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
-
 
 #### initial visualization
 plt.xlim(0.0, 1.0)
@@ -24,21 +22,27 @@ plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
-plt.show()
 ################################################################################
-
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score
 
+clf = AdaBoostClassifier()
 
+clf.fit(features_train, labels_train)
 
+pred = clf.predict(features_test)
 
+acc = accuracy_score(labels_test, pred)
 
-
+print acc
 
 try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:
     pass
+
+plt.show()
